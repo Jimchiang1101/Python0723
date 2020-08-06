1bec1b147cc9dd675ae6f3d8e9752b55bd8e333a
MB-study <gct811101@gmail.com>
2020-08-06 21:58:17 +0800

    python

--- atm/ATM.py
+++ atm/ATM.py
@@ -29,11 +29,56 @@ def save():
 
 
 def withdraw():
-    pass
+    actName = input('請輸入提款人: ')
+    money = int(input('請輸入提款金額: '))
+    # 得到存款人的account物件
+    account = None
+    for act in list:
+        for key in act.keys():
+            if key == actName:
+                account = act.get(key)
+    if account == None:
+        print('查無此人')
+    else:
+        account.withdraw(money)
 
 def transfer():
-    pass
+    fromName = input('請輸入轉帳人(from): ')
+    toName  = input('請輸入被轉帳人(to): ')
+    x = int(input('請輸入轉帳金額: '))
+    fromAccount = None
+    toAccount = None
+    for act in list:
+        for key in act.keys():
+            if key == fromName:
+                fromAccount = act.get(key)
+            if key == toName:
+                toAccount = act.get(key)
+    if fromAccount == None or toAccount == None:
+        print('查無此人')
+    else:
+        fromAccount.transfer(x, toAccount)
 
+def createAccount():
+    accountName = input('請輸入開戶人名稱: ')
+    x = int(input('請輸入開戶金額: $'))
+    account = act.Account(x)
+    dict = {accountName: account}
+    list.append(dict)
+
+def cancelAccount():
+    cancelName = input('請輸入解約人: ')
+    cancelDict = None
+    for dict in list:
+        for key in dict.keys():
+            if key == cancelName:
+                cancelDict = dict
+    if cancelDict == None:
+        print('查無此人')
+    else:
+        list.remove(cancelDict)
+        cancelAccount = cancelDict.get(cancelName)
+        print('解約人: ' + cancelName + " 解約金: $" + str(cancelAccount.getMoney()))
 
 #系統選單
 while True:
@@ -43,9 +88,11 @@ while True:
     print('2.存款')
     print('3.提款')
     print('4.轉帳')
-    print('5.離開')
+    print('5.開戶')
+    print('6.解約')
+    print('9.離開')
     print('----------')
-    no = int(input('請選擇(1~5): '))
+    no = int(input('請選擇(1~9): '))
     if no == 1:
         display()
     elif no == 2:
@@ -55,6 +102,10 @@ while True:
     elif no == 4:
         transfer()
     elif no == 5:
+        createAccount()
+    elif no == 6:
+        cancelAccount()
+    elif no == 9:
         break
     os.system('pause')#暫停(按下任意見繼續...)
 
--- atm/Account.py
+++ atm/Account.py
@@ -5,8 +5,20 @@ class Account:
     def __init__(self, x) -> None:
         self.__money = x
 
+
+    def getMoney(self):
+        return self.__money
+
+
     def save(self, x):
         self.__money += x
+
+    def withdraw(self, x):
+        self.__money -= x
+
+    def transfer(self, x, target):
+        self.withdraw(x)
+        target.save(x)
 
 
     def __str__(self) -> str:
